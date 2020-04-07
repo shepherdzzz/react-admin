f6ec6aa6093f1b78004b41c34439e79b244edbfd
shepherdzzz <xubuou@gmail.com>
2020-04-07 16:00:49 +0800

    finish LeftNav

--- src/components/left-nav/index.jsx
+++ src/components/left-nav/index.jsx
@@ -1,13 +1,75 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import { Menu } from 'antd';
+import { createFromIconfontCN } from '@ant-design/icons';
 
-import './index.less'
+import logo from '../../assets/images/logo.png';
+import menuConfig from '../../config/menuConfig';
+import './index.less';
 
-export default class LeftNav extends Component {
+const SubMenu = Menu.SubMenu;
+const IconFont = createFromIconfontCN({
+    scriptUrl: '//at.alicdn.com/t/font_1738484_7kzenqfasuu.js',
+});
+
+class LeftNav extends Component {
+    getMenuNodes = (menuList) => {
+        const path = this.props.location.pathname
+
+        return menuList.map(item => {
+            if (!item.children) {
+                return (
+                    <Menu.Item key={item.key}>
+                        <Link to={item.key}>
+                            <IconFont type={item.icon} />
+                            <span>{item.title}</span>
+                        </Link>
+                    </Menu.Item>
+                )
+            } else {
+                if (item.children.find(cItem => path.indexOf(cItem.key) === 0)) {
+                    this.openkey = item.key
+                }
+                return (
+                    <SubMenu
+                        key={item.key}
+                        title={
+                            <span>
+                                <IconFont type={item.icon} />
+                                <span>{item.title}</span>
+                            </span>
+                        }
+                    >
+                        {this.getMenuNodes(item.children)}
+                    </SubMenu>
+                )
+            }
+        })
+    }
+
     render() {
+        const selectKey = this.props.location.pathname
+        const openkey = this.openkey
+
         return (
             <div className="left-nav">
-                LeftNav
+                <Link to='/home' className='logo-link'>
+                    <img src={logo} alt="logo" />
+                    <h1>后台管理系统</h1>
+                </Link>
+                <Menu
+                    mode="inline"
+                    theme="dark"
+                    selectedKeys={[selectKey]}
+                    defaultOpenKeys={[openkey]}
+                >
+                    {
+                        this.getMenuNodes(menuConfig)
+                    }
+                </Menu>
             </div>
         )
     }
 }
+
+export default withRouter(LeftNav)
---
+++ src/config/menuConfig.js
@@ -0,0 +1,58 @@
+const menuList = [
+    {
+        title: '首页',
+        key: '/home',
+        icon: 'icon-home',
+    },
+    {
+        title: '商品',
+        key: '/products',
+        icon: 'icon-appstore',
+        children: [
+            {
+                title: '品类管理',
+                key: '/category',
+                icon: 'icon-unorderedlist',
+            },
+            {
+                title: '商品管理',
+                key: '/product',
+                icon: 'icon-wrench',
+            },
+        ]
+    },
+    {
+        title: '用户管理',
+        key: '/user',
+        icon: 'icon-user',
+    },
+    {
+        title: '角色管理',
+        key: '/role',
+        icon: 'icon-safetycertificate',
+    },
+    {
+        title: '图形图标',
+        key: '/charts',
+        icon: 'icon-areachart',
+        children: [
+            {
+                title: '柱形图',
+                key: '/charts/bar',
+                icon: 'icon-barchart',
+            },
+            {
+                title: '折线图',
+                key: '/charts/line',
+                icon: 'icon-linechart',
+            },
+            {
+                title: '饼图',
+                key: '/charts/pie',
+                icon: 'icon-piechart',
+            }
+        ]
+    }
+]
+
+export default menuList
